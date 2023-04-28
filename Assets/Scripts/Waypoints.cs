67112a70a23b4b29e49c5493a7ed7aaa287c3351
Phillip <pgiourousis@csumb.edu>
2023-04-28 15:06:03 -0700

    AI loops around track
    Fixed Ai and now it fully continues looping around the track, added a waypoint script to specifically handle waypoints, updated ai movement script for only AI movement

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    
+    //drawing out waypoint script for objects to follow in a loop 
+    private void OnDrawGizmos()
+    {   
+        //draws spheres created so object has path, which are the waypoints
+         foreach(Transform t in transform)
+         {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position,1f);
+            
+
+         }
+        
+        //draws lines of red indicating where its going from the spheres
+         Gizmos.color = Color.red;
+         for(int i=0; i<transform.childCount-1; i++)
+         {
+            Gizmos.DrawLine(transform.GetChild(i).position,transform.GetChild(i+1).position);
+         }
+         //draws loop between the waypoints essentially goes back to childobject[0]
+         Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position,transform.GetChild(0).position);
+
+    }
+
+    public Transform GetNextWaypoint(Transform currentWaypoint)
+    {
+
+        //if no waypoints exist go to first one
+        if(currentWaypoint == null )
+        {
+            return transform.GetChild(0);
+        }
+        //if this waypoint is anything but the last one then +1 and its the correct waypoint
+        if(currentWaypoint.GetSiblingIndex() < transform.childCount -1)
+        {
+        
+            return transform.GetChild(currentWaypoint.GetSiblingIndex()+1);
+        }
+        //if its last waypoint return first one
+        else
+        {
+            return transform.GetChild(0);
+        }
+
+    }
+
+}
